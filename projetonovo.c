89ef7a92a36dc0d7f3bd1a72086cf1bd250d487c
kenjisato97 <knjsato1@gmail.com>
2018-03-14 15:36:22 -0300

    arq1

---
+++ projetonovo.c
@@ -0,0 +1,53 @@
+//============================================================================
+// Name        : projetonovo.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description :
+//============================================================================
+#include <stdio.h>
+
+typedef struct
+{
+	int pavimento;
+	int quartos;
+	float areat;
+	float areac;
+
+}tCasa;
+
+typedef struct
+{
+	float area;
+	int quartos;
+	int posi;
+	float cond;
+	float vagas;
+
+}tAp;
+
+typedef struct
+{
+	float area;
+}tTer;
+
+tTer terreno[100];
+
+tCasa casa[100];
+
+tAp ap[100];
+
+
+int Cadcasa(tCasa cas)
+{
+
+}
+int main()
+{
+
+
+
+
+
+
+}
